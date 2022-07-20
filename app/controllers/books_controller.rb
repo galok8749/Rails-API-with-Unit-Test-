14699204240e13de8972f6b90893174362d2f182
alok <galok8749@gmail.com>
2022-07-20 17:42:39 +0530

    route for create book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,21 @@ class BooksController < ApplicationController
   def index
     render json: Book.all
   end
+
+  def create
+    # book = Book.create(title: "Harry potter 1", auther: "KJ Rowling");
+    book = Book.new(book_params);
+
+    if book.save
+      render json: book, status: :created
+    else
+      render json: book.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :auther)
+  end
 end
