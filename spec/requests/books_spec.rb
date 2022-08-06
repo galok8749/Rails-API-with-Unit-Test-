2d38f91a21cf25dbf1b6853ec9b2402c67051595
alok <galok8749@gmail.com>
2022-08-06 18:02:37 +0530

    Rspec API test POST/DELETE

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,13 +2,37 @@ require 'rails_helper'
 
 
 describe 'Books API', type: :request do
-  it 'returns all books' do
-    # create two book record in test database
-    FactoryBot.create(:book, title: "1984", auther: "George Orwell")
-    FactoryBot.create(:book, title: "The Time Machine", auther: "M.G Wells")
-    get '/api/v1/books'
+  describe 'GET /books' do
+    before do
+      # create two book record in test database
+      FactoryBot.create(:book, title: "1984", auther: "George Orwell")
+      FactoryBot.create(:book, title: "The Time Machine", auther: "M.G Wells")
+    end
+    it 'returns all books' do
+      get '/api/v1/books'
 
-    expect(response).to have_http_status(:success)
-    expect(JSON.parse(response.body).size).to eq(2)
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
+  end
+
+  describe 'POST /books' do
+    it 'create a new book' do
+      expect {
+        post '/api/v1/books', params: { book: {title: 'The Martian', auther: 'Andy Weir'} }
+      }.to change {Book.count}.from(0).to(1)
+
+      expect(response).to have_http_status(:created)
+    end
+  end
+
+  describe 'DELETE /books/:id' do
+    let!(:book) {FactoryBot.create(:book, title: "1984", auther: "George Orwell")}
+    it 'deletes a book' do
+      expect {
+          delete "/api/v1/books/#{book.id}"
+      }.to change {Book.count}.from(1).to(0)
+      expect(response).to have_http_status(:no_content)
+    end
   end
 end
